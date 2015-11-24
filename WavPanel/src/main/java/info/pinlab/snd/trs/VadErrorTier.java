b6f23572ea061a09131533cd0dfbf7517179b45e
Gabor Pinter <andzsinszan@gmail.com>
2015-11-24 23:28:10 +0900

    VAD Err type FSA table implemented

--- WavPanel/src/main/java/info/pinlab/snd/trs/VadErrorTier.java
+++ WavPanel/src/main/java/info/pinlab/snd/trs/VadErrorTier.java
@@ -16,14 +16,74 @@ public class VadErrorTier extends AbstractIntervalTier<VadError> {
 	BinaryTier target = null;
 	BinaryTier hypo = null;
 	
-	private static final int TARGET_ON  = 1;
-	private static final int TARGET_OFF = 2;
+	private static final int NUM_OF_ARCS = 4;
+	private static final int TARG_ON  = 1;
+	private static final int TARG_OFF = 2;
 	private static final int HYPO_ON    = 3;
 	private static final int HYPO_OFF   = 4;
-	private static final int BOTH_ON    = 5;
-	private static final int BOTH_OFF   = 6;
-	private static final int TARG_ON_HYP_OFF  = 7;
-	private static final int TARG_OFF_HYP_ON  = 8;
+//	private static final int BOTH_ON    = 5;
+//	private static final int BOTH_OFF   = 6;
+//	private static final int TARG_ON_HYP_OFF  = 7;
+//	private static final int TARG_OFF_HYP_ON  = 8;
+
+	private static final int NUM_OF_STATES = 6;
+	private static int SIL   = 0;
+	private static int VOICE = 1;
+	private static int FP_1  = 2;
+	private static int FP_2  = 3;
+	private static int FN_1  = 4;
+	private static int FN_2  = 5;
+	
+	private static final int[][] VadErrorFsa = new int[NUM_OF_STATES][NUM_OF_ARCS];
+	private static final VadError[][] VadErrorType = new VadError[NUM_OF_STATES][NUM_OF_ARCS];
+	
+	
+	static{
+		//-- Finite State Automaton for VAD errors
+		//  arcs:     Targ_on   Targ_off   Hypo_on   Hypo_off
+		//  states:                                          
+		//  SIL          FN_1                 FP_1           
+		//  FN_1                     SIL     VOICE           
+		//  FN_2                     SIL     VOICE                //-- FN_1 
+		//  FP_1        VOICE                             SIL
+		//	FP_2        VOICE                             SIL       	 
+		//  VOICE                   FP_2                 FN_2 
+		VadErrorFsa[SIL][TARG_ON] = FN_1;
+		VadErrorFsa[SIL][HYPO_ON] = FP_1;
+
+		VadErrorFsa[FN_1][TARG_OFF] = SIL;
+		VadErrorFsa[FN_1][HYPO_ON]  = VOICE;
+		VadErrorFsa[FN_2][TARG_OFF] = SIL;
+		VadErrorFsa[FN_2][HYPO_ON]  = VOICE;
+
+		VadErrorFsa[FP_1][TARG_ON]  = VOICE;
+		VadErrorFsa[FP_1][HYPO_OFF] = SIL;
+		VadErrorFsa[FP_2][TARG_ON]  = VOICE;
+		VadErrorFsa[FP_2][HYPO_OFF] = SIL;
+		
+		VadErrorFsa[VOICE][TARG_OFF] = FP_2;
+		VadErrorFsa[VOICE][HYPO_OFF] = FN_2;
+		
+		//-- VAD FSA : error types
+		//  arcs:     Targ_on   Targ_off   Hypo_on   Hypo_off
+		//  states:                                          
+		//  FN_1                      WC       FEC           
+		//  FN_2                    OVER       MSC 
+		//  FP_1         HEAD                          NDS_1
+		//	FP_2        NDS_2                           OVER       	 
+		VadErrorType[FN_1][TARG_OFF] = VadError.WC; 
+		VadErrorType[FN_1][HYPO_ON]  = VadError.FEC; 
+		VadErrorType[FN_2][TARG_OFF] = VadError.REC; 
+		VadErrorType[FN_2][HYPO_ON]  = VadError.MSC; 
+		
+		VadErrorType[FP_1][TARG_ON]  = VadError.HEAD; 
+		VadErrorType[FP_1][HYPO_OFF] = VadError.NDS_1; 
+		VadErrorType[FP_2][TARG_ON]  = VadError.NDS_2; 
+		VadErrorType[FP_2][HYPO_OFF] = VadError.TAIL;
+	}
+	
+	
+	
 	
 	
 	public <T> VadErrorTier(BinaryTier target, BinaryTier hypo){
--- WavPanel/src/main/java/info/pinlab/snd/vad/VadError.java
+++ WavPanel/src/main/java/info/pinlab/snd/vad/VadError.java
@@ -2,35 +2,51 @@ package info.pinlab.snd.vad;
 
 public enum VadError {
 	
+	
 	//-- targ  -----
 	//-- hypo  -----
 	TN (0, "sil",    "correctly detected silence"),
 	//-- targ  +++++
 	//-- hypo  +++++
 	TP (1, "speech", "correctly detected speech"),
+	
+	
+	//-- targ  ---+++++---
+	//-- hypo  -----------
+	WC (2, "w-clip",  "word clipe (voice not recognized at all)"),
+
 	//-- targ  -----------
 	//-- hypo  ---+++++---
-	FP_CLIP  (2, "extra",  "noise detected as speech within silence"),
+	NDS_1 (2, "extra",  "noise detected as speech within silence"),
+	//-- targ  +++-----+++
+	//-- hypo  +++++++++++
+	NDS_2 (2, "extra",  "noise detected as speech within silence"),
+	
+	
 	//-- targ  +++++++++++
 	//-- hypo  ++++---++++
-	FN_CLIP  (3, "clip",   "incorrectly silence mid speech"),
+	MSC  (3, "clip",   "incorrectly silence mid speech"),
 	//-- targ  +++++++----
 	//-- hypo  +++++++++++
-	FP_TAIL  (4, "tail",   "overlong speech detection"),
+	TAIL  (4, "tail",   "overlong speech detection"),
 	//-- targ  -----++++++
 	//-- hypo  --+++++++++
-	FP_HEAD  (5, "head",   "too early speech detection"),
+	HEAD  (5, "head",   "too early speech detection"),
 	//-- targ  ---++++++++
 	//-- hypo  ------+++++
-	FN_HEAD  (6, "headclip",   "clipping front part"),
+	FEC  (6, "headclip",   "front end clipping"),
 	//-- targ  ++++++++++-
 	//-- hypo  +++++++----
-	FN_TAIL  (7, "tailclip",   "clipping end part"),
+	REC  (7, "tailclip",   "rear end clipping"),
 	;
+	
+	
+	
 	
 	public final int ix;
 	public final String label;
 	public final String brief;
+
 	
 	private VadError(int ix, String label, String brief){
 		this.ix = ix;

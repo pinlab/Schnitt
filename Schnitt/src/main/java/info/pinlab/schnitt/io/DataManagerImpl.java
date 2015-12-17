698515ef0b161c9eac2b745b398e609513ce5366
Gabor Pinter <andzsinszan@gmail.com>
2015-12-17 21:52:02 +0900

    Naoi kun's IO classes for textGrid added

--- Schnitt/src/main/java/info/pinlab/schnitt/io/DataManagerImpl.java
+++ Schnitt/src/main/java/info/pinlab/schnitt/io/DataManagerImpl.java
@@ -11,85 +11,86 @@ import info.pinlab.snd.vad.BinaryTargetTier;
 
 public class DataManagerImpl implements DataManager {
 
-	
-	@Override
-	public AudioWithTiers readFile(String wavPath) throws IOException{
-		WavClip wav = new WavClip(wavPath);
-		AudioWithTiers awt = new AudioWithTiers(wav);
-		
-		
-		String tierPath = FileNameManip.dirNameToTextGridFilePath(wavPath);
-		if(tierPath!=null){
-			System.out.println(tierPath);
-			BinaryTargetTier target = DataToBinaryTier.TextGridToBinaryTier(tierPath);
-			awt.setTarget(target);
-		}
-		return awt;
-	}
+    
+    @Override
+    public AudioWithTiers readFile(String wavPath) throws IOException{
+        WavClip wav = new WavClip(wavPath);
+        AudioWithTiers awt = new AudioWithTiers(wav);
+        
+        
+        String tierPath = FileNameManip.dirNameToTextGridFilePath(wavPath);
+        if(tierPath!=null){
+            System.out.println(tierPath);
+            BinaryTargetTier target = DataToBinaryTier.TextGridToBinaryTier(tierPath);
+            awt.setTarget(target);
+        }
+        return awt;
+    }
 
-	
-	@Override
-	public List<AudioWithTiers> parseDir(String dir, boolean isRrecursive){
-		List<AudioWithTiers> awts = new ArrayList<AudioWithTiers>();
-		
-		File rootDir = new File(dir);
-		if(!rootDir.isDirectory()){
-			return awts;
-		}
+    
+    @Override
+    public List<AudioWithTiers> parseDir(String dir, boolean isRrecursive){
+        List<AudioWithTiers> awts = new ArrayList<AudioWithTiers>();
+        
+        File rootDir = new File(dir);
+        if(!rootDir.isDirectory()){
+            return awts;
+        }
 
-		//-- Check wav files
-		for(File f: rootDir.listFiles()){
-			String fname = f.getName().toLowerCase();
-			if(f.isFile() && fname.endsWith(".wav")){
-				//-- wav file
-				System.out.println(f.getName());
-				try{
-					AudioWithTiers awt = readFile(f.getAbsolutePath());
-					awts.add(awt);
-				}catch(IOException e){
-					//-- pass
-				}
-			}
-		}
-		return awts;
-	}
-	
-	@Override
-	public List<AudioWithTiers> parseDir(String dir) {
-		return parseDir(dir, false);
-	}
-	
-	
-	
-	static void checkWithGui(AudioWithTiers awt) throws Exception{
-		WavClip wav = awt.getWav();
-		Application app = new Application(wav);
-		app.start();
-	}
+        //-- Check wav files
+        for(File f: rootDir.listFiles()){
+            String fname = f.getName().toLowerCase();
+            if(f.isFile() && fname.endsWith(".wav")){
+                //-- wav file
+                System.out.println(f.getName());
+                try{
+                    AudioWithTiers awt = readFile(f.getAbsolutePath());
+                    awts.add(awt);
+                }catch(IOException e){
+                    //-- pass
+                }
+            }
+        }
+        return awts;
+    }
+    
+    @Override
+    public List<AudioWithTiers> parseDir(String dir) {
+        return parseDir(dir, false);
+    }
+    
+    
+    
+    static void checkWithGui(AudioWithTiers awt) throws Exception{
+        WavClip wav = awt.getWav();
+        Application app = new Application(wav);
+        app.start();
+    }
 
-	
-	
-	public static void main (String [] args) throws Exception{
-		DataManagerImpl dataManager = new DataManagerImpl();
-		
-//		String wavPath = "C:\\Users\\Naoi\\Desktop\\VAD-ay2015-uw-pre-Working\\1376470_ay2015-uw-pre-TaskSet05_005_1_[20150824-150909].wav";
-		String wavPath = "C:\\Users\\Naoi\\Desktop\\VAD-ay2015-uw-pre-Working\\1392739_ay2015-uw-pre-TaskSet05_006_1_[20150824-150818].wav";
-		
-		AudioWithTiers awt = dataManager.readFile(wavPath);
-//		System.out.println("Wav -> Target load test");
-		
-		
-		String dir = "C:\\Users\\Naoi\\Desktop\\VAD-ay2015-uw-pre-Working\\";
-//		List<AudioWithTiers> awts = dataManager.parseDir(dir);
-//		System.out.println(awts.size());
-		
-//		Application app = new Application(awt);
-//		app.start();
+    
+    
+    
+    public static void main (String [] args) throws Exception{
+        DataManagerImpl dataManager = new DataManagerImpl();
+        
+        String wavPath = "C:\\Users\\Naoi\\Desktop\\VAD-ay2015-uw-pre-Working\\1376470_ay2015-uw-pre-TaskSet05_003_1_[20150824-150700].wav";
+        
+        AudioWithTiers awt = dataManager.readFile(wavPath);
+        
+        String dir = "C:\\Users\\Naoi\\Desktop\\VAD-ay2015-uw-pre-Working\\";
+//        List<AudioWithTiers> awts = dataManager.parseDir(dir);
+//        System.out.println(awts.size());
+        
+        Application app = new Application(awt);
+        app.start();
 
-		
-	}
+        
+    }
 
 
 
 
 }
+
+
+
--- Schnitt/src/main/java/info/pinlab/schnitt/io/FileNameManip.java
+++ Schnitt/src/main/java/info/pinlab/schnitt/io/FileNameManip.java
@@ -5,49 +5,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileNameManip {
-	
-	public static String getFileName(String fullpath){
-		int index = fullpath.lastIndexOf('\\');
-		return fullpath.substring(index+1, fullpath.length());
-	}
-	
-	public static String getDirName(String fullpath){
-		int index = fullpath.lastIndexOf('\\');
-		return fullpath.substring(0, index+1);
-	}
-	
-	static final String regex = "(\\[|\\])";
-	/**
-	 * ignores case
-	 * 
-	 * @param wavfilename
-	 * @return
-	 */
-	public static List<String> getTextGridFileNameCandidates(String wavfilename){
-		List<String> textgridfilesnames = new ArrayList<String>();
-		
-		textgridfilesnames.add(wavfilename.replaceAll(".wav",".TextGrid").replaceAll(regex, "_"));
-		textgridfilesnames.add(wavfilename.replaceAll(".wav",".TEXTGRID").replaceAll(regex, "_"));
-		textgridfilesnames.add(wavfilename.replaceAll(".wav",".textgrid").replaceAll(regex, "_"));
-		
-		return textgridfilesnames;
-	}
+    
+    public static String getFileName(String fullpath){
+        int index = fullpath.lastIndexOf('\\');
+        return fullpath.substring(index+1, fullpath.length());
+    }
+    
+    public static String getDirName(String fullpath){
+        int index = fullpath.lastIndexOf('\\');
+        return fullpath.substring(0, index+1);
+    }
+    
+    static final String regex = "(\\[|\\])";
+    /**
+     * ignores case
+     *
+     * @param wavfilename
+     * @return
+     */
+    public static List<String> getTextGridFileNameCandidates(String wavfilename){
+        List<String> textgridfilesnames = new ArrayList<String>();
+        
+        textgridfilesnames.add(wavfilename.replaceAll(".wav",".TextGrid").replaceAll(regex, "_"));
+        textgridfilesnames.add(wavfilename.replaceAll(".wav",".TEXTGRID").replaceAll(regex, "_"));
+        textgridfilesnames.add(wavfilename.replaceAll(".wav",".textgrid").replaceAll(regex, "_"));
+        textgridfilesnames.add(wavfilename.replaceAll(".WAV",".textgrid").replaceAll(regex, "_"));
+        textgridfilesnames.add(wavfilename.replaceAll(".WAV",".TextGrid").replaceAll(regex, "_"));
+        
+        return textgridfilesnames;
+    }
 
-	
-	public static String dirNameToTextGridFilePath(String dirName){
-		String WavFileName = FileNameManip.getFileName(dirName);
-		
-		List<String> tgNameCandidates = FileNameManip.getTextGridFileNameCandidates(WavFileName);
-		
-		for(String tgNameCandidate: tgNameCandidates){
-			String path = FileNameManip.getDirName(dirName) + tgNameCandidate;
-//			System.out.println("PATH" + path);
-			File filePath = new File(path);
-			if(filePath.exists() && filePath.isFile()){
-				return filePath.getAbsolutePath();
-			}
-		}
-		return null; //-- no corresponding .textGrid 
-	}
+    
+    public static String dirNameToTextGridFilePath(String dirName){
+        String WavFileName = FileNameManip.getFileName(dirName);
+        
+        List<String> tgNameCandidates = FileNameManip.getTextGridFileNameCandidates(WavFileName);
+        
+        for(String tgNameCandidate: tgNameCandidates){
+            String path = FileNameManip.getDirName(dirName) + tgNameCandidate;
+//            System.out.println("PATH" + path);
+            File filePath = new File(path);
+            if(filePath.exists() && filePath.isFile()){
+                return filePath.getAbsolutePath();
+            }
+        }
+        return null; //-- no corresponding .textGrid
+    }
 }
+
 
